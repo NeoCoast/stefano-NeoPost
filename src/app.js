933a81515902a33f541ae5966078600f551cd997
Stefano <gazzottistefano@gmail.com>
2026-02-25 13:20:42 -0300

    feat: add post routes (create + edit) and mount in app

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ const passport = require('./middlewares/passport');
 const app = express();
 
 const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
 
 app.use(express.json());
 app.use(passport.initialize());
 
 app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
 module.exports = app;
